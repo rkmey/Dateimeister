331eae536ceeaa87d71c31bd8cdf7161bcd41018
Rolf Meyer <rkmeyer@t-online.de>
2025-02-27 20:07:58 +0100

    Diatisch: process list (cont.)

--- Dateimeister_processlist.py
+++ Dateimeister_processlist.py
@@ -45,6 +45,7 @@ class MyProcesslistWindow:
 
         self.dict_processlist = dict_processlist
         self.debug = debug
+        self.procstep_selected = None
 
         # resizable font
         self.text_font = Font(family="Helvetica", size=6)
@@ -65,18 +66,30 @@ class MyProcesslistWindow:
         self.Label_proc_ctr.configure(text='Num Procteps: 0')
 
         self.Frame_process_list = tk.Frame(self.root)
-        self.Frame_process_list.place(relx=.01, rely= self.frame_labels_height + .01, relheight= 1- self.frame_labels_height, relwidth=0.98)
+        self.Frame_process_list.place(relx=.01, rely= self.frame_labels_height + .01, relheight= 1 - self.frame_labels_height -.02, relwidth=0.98)
         self.Frame_process_list.configure(relief='flat')
         self.Frame_process_list.configure(background="#d9d9d9") if self.debug else True # uncomment for same colour as window (default) or depend on debug
 
         # listbox for procsteps
+        self.listbox_procsteps_relheight = .9
+        self.listbox_procsteps_relwidth  = .9
         self.listbox_procsteps_var = tk.StringVar()
         self.listbox_procsteps = tk.Listbox(self.Frame_process_list)
-        self.listbox_procsteps.place(relx=.0, rely=0, relheight=.9, relwidth=0.9)
+        self.listbox_procsteps.place(relx=.0, rely=0, relheight= self.listbox_procsteps_relheight, relwidth=self.listbox_procsteps_relwidth)
         self.listbox_procsteps.configure(font=self.text_font)
         self.listbox_procsteps.configure(selectmode='single')
         self.listbox_procsteps.configure(listvariable=self.listbox_procsteps_var)
 
+        # Scrollbars
+        VI_PROCSTEPS = tk.Scrollbar(self.Frame_process_list, orient= VERTICAL)
+        VI_PROCSTEPS.place(relx = self.listbox_procsteps_relwidth, rely = 0, relheight = self.listbox_procsteps_relheight, relwidth = .01, anchor = tk.NW)
+        VI_PROCSTEPS.config(command = self.listbox_procsteps.yview)
+        self.listbox_procsteps.config(yscrollcommand = VI_PROCSTEPS.set)
+        HI_PROCSTEPS = tk.Scrollbar(self.Frame_process_list, orient= HORIZONTAL)
+        HI_PROCSTEPS.place(relx = 0, rely = self.listbox_procsteps_relheight, relheight = 0.03, relwidth = self.listbox_procsteps_relwidth, anchor = tk.NW)
+        HI_PROCSTEPS.config(command = self.listbox_procsteps.xview)
+        self.listbox_procsteps.config(xscrollcommand = HI_PROCSTEPS.set)
+        self.listbox_procsteps.bind('<Double-1>', self.listbox_procsteps_double)
 
     def on_configure(self, event):
         x = str(event.widget)
@@ -91,6 +104,16 @@ class MyProcesslistWindow:
                 fontsize_use = int(.8 * min(12.0, l_height * .75))
                 print(f"The height of Toplevel is {self.height}, label height is {l_height} set fontsize to {fontsize_use}") if self.debug else True
                 self.text_font.configure(size=fontsize_use)                
+
+    def listbox_procsteps_double(self, event = None):
+        selected_indices = self.listbox_procsteps_double.curselection()
+        if not selected_indices:
+            self.procstep_selected = None
+            messagebox.showwarning("Warning", "Listbox Indir: nothing selected", parent = self.Frame_indir)
+        else:
+            procstep = ",".join([self.listbox_procsteps_double.get(i) for i in selected_indices]) # because listbox has single selection
+            print("procstep selected is: " + procstep) if self.debug else True
+            self.procstep_selected = procstep
 
     def close_handler(self): #calles when window is closing:
         self.root.destroy()
