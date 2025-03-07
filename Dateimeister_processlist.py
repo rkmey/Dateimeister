9acedec40bd75142d87ea51bd253b4e254fc1691
Rolf <155853663+rkmey@users.noreply.github.com>
2025-03-07 10:57:17 +0100

    Diatisch: listboxes for actual and undone processes

--- Dateimeister_processlist.py
+++ Dateimeister_processlist.py
@@ -66,22 +66,57 @@ class MyProcesslistWindow:
         self.Label_proc_ctr.configure(font=self.text_font)
         self.Label_proc_ctr.configure(text='Num Procteps: 0')
 
+        # Frame for process_hist
+        self.Frame_process_hist = tk.Frame(self.root)
+        self.Frame_process_hist.place(relx=.01, rely= self.frame_labels_height + .01, relheight= 1 - self.frame_labels_height -.02, relwidth=0.32)
+        self.Frame_process_hist.configure(relief='flat')
+        self.Frame_process_hist.configure(background="#d9d9d9") if self.debug else True # uncomment for same colour as window (default) or depend on debug
+
+        # listbox for process_hist
+        self.listbox_process_hist_relheight = .9
+        self.listbox_process_hist_relwidth  = .9
+        self.listbox_process_hist_var = tk.StringVar()
+        self.listbox_process_hist = tk.Listbox(self.Frame_process_hist)
+        self.listbox_process_hist.place(relx=.0, rely=0, relheight= self.listbox_process_hist_relheight, relwidth=self.listbox_process_hist_relwidth)
+        self.listbox_process_hist.configure(font=self.text_font)
+        self.listbox_process_hist.configure(selectmode='single')
+        self.listbox_process_hist.configure(listvariable=self.listbox_process_hist_var)
+
+        # Scrollbars for process_hist
+        self.Frame_process_hist.update()
+        w = self.Frame_process_hist.winfo_width()
+        h = self.Frame_process_hist.winfo_height()
+        f = 4000 / w # for 4k screen appr. 1
+        b = f * w / 50000
+        r = w / h
+        print("frame width / height = {:d}, {:d}".format(w, h)) if self.debug else True
+        self.vi_process_hist = tk.Scrollbar(self.Frame_process_hist, orient= VERTICAL)
+        self.vi_process_hist.place(relx = self.listbox_process_hist_relwidth, rely = 0, relheight = self.listbox_process_hist_relheight, relwidth = min(max(b, 0.005 * f), .03), anchor = tk.NW)
+        self.vi_process_hist.config(command = self.listbox_process_hist.yview)
+        self.listbox_process_hist.config(yscrollcommand = self.vi_process_hist.set)
+        self.hi_process_hist = tk.Scrollbar(self.Frame_process_hist, orient= HORIZONTAL)
+        self.hi_process_hist.place(relx = 0, rely = self.listbox_process_hist_relheight, relheight = min(max(b * r, 0.005 * f * r), .03 * r), relwidth = self.listbox_process_hist_relwidth, anchor = tk.NW)
+        self.hi_process_hist.config(command = self.listbox_process_hist.xview)
+        self.listbox_process_hist.config(xscrollcommand = self.hi_process_hist.set)
+        self.listbox_process_hist.bind('<Double-1>', self.listbox_process_hist_double)
+
+        # Frame for process_list
         self.Frame_process_list = tk.Frame(self.root)
-        self.Frame_process_list.place(relx=.01, rely= self.frame_labels_height + .01, relheight= 1 - self.frame_labels_height -.02, relwidth=0.32)
+        self.Frame_process_list.place(relx=.34, rely= self.frame_labels_height + .01, relheight= 1 - self.frame_labels_height -.02, relwidth=0.32)
         self.Frame_process_list.configure(relief='flat')
         self.Frame_process_list.configure(background="#d9d9d9") if self.debug else True # uncomment for same colour as window (default) or depend on debug
 
-        # listbox for procsteps
-        self.listbox_procsteps_relheight = .9
-        self.listbox_procsteps_relwidth  = .9
-        self.listbox_procsteps_var = tk.StringVar()
-        self.listbox_procsteps = tk.Listbox(self.Frame_process_list)
-        self.listbox_procsteps.place(relx=.0, rely=0, relheight= self.listbox_procsteps_relheight, relwidth=self.listbox_procsteps_relwidth)
-        self.listbox_procsteps.configure(font=self.text_font)
-        self.listbox_procsteps.configure(selectmode='single')
-        self.listbox_procsteps.configure(listvariable=self.listbox_procsteps_var)
+        # listbox for process_list
+        self.listbox_process_list_relheight = .9
+        self.listbox_process_list_relwidth  = .9
+        self.listbox_process_list_var = tk.StringVar()
+        self.listbox_process_list = tk.Listbox(self.Frame_process_list)
+        self.listbox_process_list.place(relx=.0, rely=0, relheight= self.listbox_process_list_relheight, relwidth=self.listbox_process_list_relwidth)
+        self.listbox_process_list.configure(font=self.text_font)
+        self.listbox_process_list.configure(selectmode='single')
+        self.listbox_process_list.configure(listvariable=self.listbox_process_list_var)
 
-        # Scrollbars
+        # Scrollbars for process_list
         self.Frame_process_list.update()
         w = self.Frame_process_list.winfo_width()
         h = self.Frame_process_list.winfo_height()
@@ -89,15 +124,50 @@ class MyProcesslistWindow:
         b = f * w / 50000
         r = w / h
         print("frame width / height = {:d}, {:d}".format(w, h)) if self.debug else True
-        self.VI_PROCSTEPS = tk.Scrollbar(self.Frame_process_list, orient= VERTICAL)
-        self.VI_PROCSTEPS.place(relx = self.listbox_procsteps_relwidth, rely = 0, relheight = self.listbox_procsteps_relheight, relwidth = min(max(b, 0.005 * f), .03), anchor = tk.NW)
-        self.VI_PROCSTEPS.config(command = self.listbox_procsteps.yview)
-        self.listbox_procsteps.config(yscrollcommand = self.VI_PROCSTEPS.set)
-        self.HI_PROCSTEPS = tk.Scrollbar(self.Frame_process_list, orient= HORIZONTAL)
-        self.HI_PROCSTEPS.place(relx = 0, rely = self.listbox_procsteps_relheight, relheight = min(max(b * r, 0.005 * f * r), .03 * r), relwidth = self.listbox_procsteps_relwidth, anchor = tk.NW)
-        self.HI_PROCSTEPS.config(command = self.listbox_procsteps.xview)
-        self.listbox_procsteps.config(xscrollcommand = self.HI_PROCSTEPS.set)
-        self.listbox_procsteps.bind('<Double-1>', self.listbox_procsteps_double)
+        self.vi_process_list = tk.Scrollbar(self.Frame_process_list, orient= VERTICAL)
+        self.vi_process_list.place(relx = self.listbox_process_list_relwidth, rely = 0, relheight = self.listbox_process_list_relheight, relwidth = min(max(b, 0.005 * f), .03), anchor = tk.NW)
+        self.vi_process_list.config(command = self.listbox_process_list.yview)
+        self.listbox_process_list.config(yscrollcommand = self.vi_process_list.set)
+        self.hi_process_list = tk.Scrollbar(self.Frame_process_list, orient= HORIZONTAL)
+        self.hi_process_list.place(relx = 0, rely = self.listbox_process_list_relheight, relheight = min(max(b * r, 0.005 * f * r), .03 * r), relwidth = self.listbox_process_list_relwidth, anchor = tk.NW)
+        self.hi_process_list.config(command = self.listbox_process_list.xview)
+        self.listbox_process_list.config(xscrollcommand = self.hi_process_list.set)
+        self.listbox_process_list.bind('<Double-1>', self.listbox_process_list_double)
+
+        # Frame for process_undo
+        self.Frame_process_undo = tk.Frame(self.root)
+        self.Frame_process_undo.place(relx=.67, rely= self.frame_labels_height + .01, relheight= 1 - self.frame_labels_height -.02, relwidth=0.32)
+        self.Frame_process_undo.configure(relief='flat')
+        self.Frame_process_undo.configure(background="#d9d9d9") if self.debug else True # uncomment for same colour as window (default) or depend on debug
+
+        # listbox for process_undo
+        self.listbox_process_undo_relheight = .9
+        self.listbox_process_undo_relwidth  = .9
+        self.listbox_process_undo_var = tk.StringVar()
+        self.listbox_process_undo = tk.Listbox(self.Frame_process_undo)
+        self.listbox_process_undo.place(relx=.0, rely=0, relheight= self.listbox_process_undo_relheight, relwidth=self.listbox_process_undo_relwidth)
+        self.listbox_process_undo.configure(font=self.text_font)
+        self.listbox_process_undo.configure(selectmode='single')
+        self.listbox_process_undo.configure(listvariable=self.listbox_process_undo_var)
+
+        # Scrollbars for process_undo
+        self.Frame_process_undo.update()
+        w = self.Frame_process_undo.winfo_width()
+        h = self.Frame_process_undo.winfo_height()
+        f = 4000 / w # for 4k screen appr. 1
+        b = f * w / 50000
+        r = w / h
+        print("frame width / height = {:d}, {:d}".format(w, h)) if self.debug else True
+        self.vi_process_undo = tk.Scrollbar(self.Frame_process_undo, orient= VERTICAL)
+        self.vi_process_undo.place(relx = self.listbox_process_undo_relwidth, rely = 0, relheight = self.listbox_process_undo_relheight, relwidth = min(max(b, 0.005 * f), .03), anchor = tk.NW)
+        self.vi_process_undo.config(command = self.listbox_process_undo.yview)
+        self.listbox_process_undo.config(yscrollcommand = self.vi_process_undo.set)
+        self.hi_process_undo = tk.Scrollbar(self.Frame_process_undo, orient= HORIZONTAL)
+        self.hi_process_undo.place(relx = 0, rely = self.listbox_process_undo_relheight, relheight = min(max(b * r, 0.005 * f * r), .03 * r), relwidth = self.listbox_process_undo_relwidth, anchor = tk.NW)
+        self.hi_process_undo.config(command = self.listbox_process_undo.xview)
+        self.listbox_process_undo.config(xscrollcommand = self.hi_process_undo.set)
+        self.listbox_process_undo.bind('<Double-1>', self.listbox_process_undo_double)
+
         self.initialized = True
 
     def on_configure(self, event):
@@ -116,24 +186,49 @@ class MyProcesslistWindow:
                 self.text_font.configure(size=fontsize_use)                
             if self.initialized:
                 # configure scrollbars
-                self.Frame_process_list.update()
-                w = self.Frame_process_list.winfo_width()
-                h = self.Frame_process_list.winfo_height()
+                self.Frame_process_hist.update()
+                # the following parameters are the same for all listboxes in processlist window
+                w = self.Frame_process_hist.winfo_width()
+                h = self.Frame_process_hist.winfo_height()
                 f = 4000 / w # for 4k screen appr. 1
                 b = f * w / 50000
                 r = w / h
                 print("frame width / height = {:d}, {:d}".format(w, h)) if self.debug else True
-                self.VI_PROCSTEPS.place(relx = self.listbox_procsteps_relwidth, rely = 0, relheight = self.listbox_procsteps_relheight, relwidth = min(max(b, 0.005 * f), .02), anchor = tk.NW)
-                self.HI_PROCSTEPS.place(relx = 0, rely = self.listbox_procsteps_relheight, relheight = min(max(b * r, 0.005* f * r), .02 * r), relwidth = self.listbox_procsteps_relwidth, anchor = tk.NW)
-        
+                self.vi_process_hist.place(relx = self.listbox_process_hist_relwidth, rely = 0, relheight = self.listbox_process_hist_relheight, relwidth = min(max(b, 0.005 * f), .02), anchor = tk.NW)
+                self.hi_process_hist.place(relx = 0, rely = self.listbox_process_hist_relheight, relheight = min(max(b * r, 0.005* f * r), .02 * r), relwidth = self.listbox_process_hist_relwidth, anchor = tk.NW)
+                self.vi_process_list.place(relx = self.listbox_process_hist_relwidth, rely = 0, relheight = self.listbox_process_hist_relheight, relwidth = min(max(b, 0.005 * f), .02), anchor = tk.NW)
+                self.hi_process_list.place(relx = 0, rely = self.listbox_process_hist_relheight, relheight = min(max(b * r, 0.005* f * r), .02 * r), relwidth = self.listbox_process_hist_relwidth, anchor = tk.NW)
+                self.vi_process_undo.place(relx = self.listbox_process_hist_relwidth, rely = 0, relheight = self.listbox_process_hist_relheight, relwidth = min(max(b, 0.005 * f), .02), anchor = tk.NW)
+                self.hi_process_undo.place(relx = 0, rely = self.listbox_process_hist_relheight, relheight = min(max(b * r, 0.005* f * r), .02 * r), relwidth = self.listbox_process_hist_relwidth, anchor = tk.NW)
+       
 
-    def listbox_procsteps_double(self, event = None):
-        selected_indices = self.listbox_procsteps_double.curselection()
+    def listbox_process_hist_double(self, event = None):
+        selected_indices = self.listbox_process_hist_double.curselection()
         if not selected_indices:
             self.procstep_selected = None
-            messagebox.showwarning("Warning", "Listbox Indir: nothing selected", parent = self.Frame_indir)
+            messagebox.showwarning("Warning", "Listbox process_hist: nothing selected", parent = self.Frame_process_hist)
         else:
-            procstep = ",".join([self.listbox_procsteps_double.get(i) for i in selected_indices]) # because listbox has single selection
+            procstep = ",".join([self.listbox_process_hist_double.get(i) for i in selected_indices]) # because listbox has single selection
+            print("procstep selected is: " + procstep) if self.debug else True
+            self.procstep_selected = procstep
+
+    def listbox_process_list_double(self, event = None):
+        selected_indices = self.listbox_process_list_double.curselection()
+        if not selected_indices:
+            self.procstep_selected = None
+            messagebox.showwarning("Warning", "Listbox process_list: nothing selected", parent = self.Frame_process_list)
+        else:
+            procstep = ",".join([self.listbox_process_list_double.get(i) for i in selected_indices]) # because listbox has single selection
+            print("procstep selected is: " + procstep) if self.debug else True
+            self.procstep_selected = procstep
+
+    def listbox_process_undo_double(self, event = None):
+        selected_indices = self.listbox_process_list_double.curselection()
+        if not selected_indices:
+            self.procstep_selected = None
+            messagebox.showwarning("Warning", "Listbox process_undo: nothing selected", parent = self.Frame_process_undo)
+        else:
+            procstep = ",".join([self.listbox_process_list_double.get(i) for i in selected_indices]) # because listbox has single selection
             print("procstep selected is: " + procstep) if self.debug else True
             self.procstep_selected = procstep
 
